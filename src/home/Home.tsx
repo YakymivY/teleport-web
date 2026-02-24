a60c4b1fb3c0e41217ce9016130a1e514a387db6
Yuriy Yakymiv <yuriyyakymiv@Yuriys-MacBook-Air.local>
2026-02-24 16:09:55 +0200

    download file POC

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,11 +1,13 @@
 import { Single } from './Single';
 import { Multi } from './Multi';
+import { Download } from './Download';
 
 export function Home() {
   return (
     <>
       <Single />
       <Multi />
+      <Download />
     </>
   );
 }
