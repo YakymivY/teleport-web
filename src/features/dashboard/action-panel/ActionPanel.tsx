74f9ddc4a801f7c41a83700ebc6277311c67cebb
Yuriy Yakymiv <yuriyyakymiv@Yuriys-MacBook-Air.local>
2026-03-05 16:52:03 +0200

    dashboard elements styling

--- src/features/dashboard/action-panel/ActionPanel.tsx
+++ src/features/dashboard/action-panel/ActionPanel.tsx
@@ -1,7 +1,15 @@
+import plusIcon from '../../../assets/plus-icon.png';
 import './ActionPanel.css';
 
 export function ActionPanel() {
+
   return (
-    <div className="action-panel-container"></div>
+    <div className="action-panel-container">
+      <button className="action-panel-button">
+        <img src={plusIcon} alt="plus" className="action-panel-button-icon" />
+      </button>
+      <button className="action-panel-button"></button>
+      <button className="action-panel-button"></button>
+    </div>
   );
 }
