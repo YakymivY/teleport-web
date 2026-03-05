0400b8d57511a68a6d60d95742df74e34ba58ae5
Yuriy Yakymiv <yuriyyakymiv@Yuriys-MacBook-Air.local>
2026-03-05 22:09:21 +0200

    logout buttons added

--- src/features/dashboard/action-panel/ActionPanel.tsx
+++ src/features/dashboard/action-panel/ActionPanel.tsx
@@ -1,27 +1,30 @@
 import { useRef, useState } from 'react';
-import plusIcon from '../../../assets/plus-icon.png';
 import { Modal } from '../../../ui/Modal';
+import plusIcon from '../../../assets/plus-icon.png';
 import './ActionPanel.css';
 
 export function ActionPanel() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const plusButtonRef = useRef<HTMLButtonElement | null>(null);
+  const logoutButtonRef = useRef<HTMLButtonElement | null>(null);
 
   return (
     <div className="action-panel-container">
-      <button
-        ref={plusButtonRef}
-        className="action-panel-button"
-        onClick={() => setIsModalOpen(true)}
-      >
+      <button className="action-panel-button">
         <img src={plusIcon} alt="plus" className="action-panel-button-icon" />
       </button>
       <button className="action-panel-button"></button>
-      <button className="action-panel-button">
+      <button ref={logoutButtonRef} className="action-panel-button" onClick={() => setIsModalOpen(true)}>
         <span className="action-panel-button-letter">L</span>
       </button>
-      <Modal anchorRef={plusButtonRef} isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        Modal content
+      <Modal anchorRef={logoutButtonRef} isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <div className="action-panel-modal-buttons">
+          <button className="action-panel-button action-panel-modal-button action-panel-modal-button--primary">
+            <span className="action-panel-button-letter">log out</span>
+          </button>
+          <button className="action-panel-button action-panel-modal-button action-panel-modal-button--secondary">
+            <span className="action-panel-button-letter">log out everywhere</span>
+          </button>
+        </div>
       </Modal>
     </div>
   );
