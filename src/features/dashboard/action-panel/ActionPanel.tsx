e4bcdcddee2620bb068deb5939210700e65c5070
Yuriy Yakymiv <yuriyyakymiv@Yuriys-MacBook-Air.local>
2026-04-20 12:47:24 +0300

    traffic button added

--- src/features/dashboard/action-panel/ActionPanel.tsx
+++ src/features/dashboard/action-panel/ActionPanel.tsx
@@ -1,13 +1,14 @@
 import { UploadButton } from './components/upload-button/UploadButton';
-import './ActionPanel.css';
 import { LogoutButton } from './components/logout-button/LogoutButton';
+import { TrafficButton } from './components/traffice-button/TrafficButton';
+import './ActionPanel.css';
 
 export function ActionPanel() {
 
   return (
     <div className="action-panel-container">
       <UploadButton />
-      <button className="action-panel-button"></button>
+      <TrafficButton />
       <LogoutButton />
     </div>
   );
